71119da5e98a5b999e3745ceb8ae0782cff8cf8d
濱田早由里 <sayuri@hamadasayorisatonoMacBook-Pro.local>
2019-12-03 11:06:16 +0900

    [fix] エラー修正

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,5 @@
 
 <script>
 export default {
-  created() {
-    console.log(this.$store.state.count)
-    this.$store.commit('increment')
-  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,6 @@ export default {
   name: 'home',
   components: {
     workList
-  },
+  }
 }
 </script>
